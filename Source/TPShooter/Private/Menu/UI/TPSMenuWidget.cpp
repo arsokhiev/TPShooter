ce1d589ea282984c4a9a83542c261ef67dfaa2f3
Arsen <arsensokhiev@gmail.com>
2023-05-16 02:48:15 +0300

    Selecting a level in the menu
    skipped file loaded

--- Source/TPShooter/Private/Menu/UI/TPSMenuWidget.cpp
+++ Source/TPShooter/Private/Menu/UI/TPSMenuWidget.cpp
@@ -3,7 +3,9 @@
 
 #include "Menu/UI/TPSMenuWidget.h"
 #include "TPSGameInstance.h"
+#include "TPSLevelItemWidget.h"
 #include "Components/Button.h"
+#include "Components/HorizontalBox.h"
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
 
@@ -22,25 +24,74 @@ void UTPSMenuWidget::NativeOnInitialized()
 	{
 		QuitGameButton->OnClicked.AddDynamic(this, &UTPSMenuWidget::OnQuitGameHandle);
 	}
+
+	InitLevelItems();
 }
 
 void UTPSMenuWidget::OnStartGameHandle()
 {
-	if (!GetWorld()) return;
-
-	const auto GameInstance = GetWorld()->GetGameInstance<UTPSGameInstance>();
-	if (!GameInstance) return;
-
-	if (GameInstance->GetStartupLevelName().IsNone())
-	{
-		UE_LOG(LogTPSMenuWidget, Error, TEXT("Level name is NONE"));
-		return;
-	}
+	const auto TPSGameInstance = GetTPSGameInstance();
+	if (!TPSGameInstance) return;
 	
-	UGameplayStatics::OpenLevel(this, GameInstance->GetStartupLevelName());
+	UGameplayStatics::OpenLevel(this, TPSGameInstance->GetStartupLevel().LevelName);
 }
 
 void UTPSMenuWidget::OnQuitGameHandle()
 {
 	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
+
+void UTPSMenuWidget::InitLevelItems()
+{
+	const auto TPSGameInstance = GetTPSGameInstance();
+	if (!TPSGameInstance) return;
+
+	checkf(TPSGameInstance->GetLevelsData().Num() != 0, TEXT("Levels data must not be empty"));
+
+	if (!LevelItemsBox) return;
+	LevelItemsBox->ClearChildren();
+
+	for (auto LevelData : TPSGameInstance->GetLevelsData())
+	{
+		const auto LevelItemWidget = CreateWidget<UTPSLevelItemWidget>(GetWorld(), LevelItemWidgetClass);
+		if (!LevelItemWidget) continue;
+
+		LevelItemWidget->SetLevelData(LevelData);
+		LevelItemWidget->OnLevelSelected.AddUObject(this, &UTPSMenuWidget::OnLevelSelectedHandle);
+
+		LevelItemsBox->AddChild(LevelItemWidget);
+		LevelItemWidgets.Add(LevelItemWidget);
+	}
+
+	if (TPSGameInstance->GetStartupLevel().LevelName.IsNone())
+	{
+		OnLevelSelectedHandle(TPSGameInstance->GetLevelsData()[0]);
+	}
+	else
+	{
+		OnLevelSelectedHandle(TPSGameInstance->GetStartupLevel());
+	}
+}
+
+void UTPSMenuWidget::OnLevelSelectedHandle(const FLevelData& Data)
+{
+	const auto TPSGameInstance = GetTPSGameInstance();
+	if (!TPSGameInstance) return;
+
+	TPSGameInstance->SetStartupLevel(Data);
+
+	for (auto LevelItemWidget : LevelItemWidgets)
+	{
+		if (LevelItemWidget)
+		{
+			const auto IsSelected = Data.LevelName == LevelItemWidget->GetLevelData().LevelName;
+			LevelItemWidget->SetSelected(IsSelected);
+		}
+	}
+}
+
+UTPSGameInstance* UTPSMenuWidget::GetTPSGameInstance() const
+{
+	if (!GetWorld()) return nullptr;
+	return GetWorld()->GetGameInstance<UTPSGameInstance>();
 }
